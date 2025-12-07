746094f8dace2041b34966366cc166314284c282
Miguel Ángel Torres Varo <emeateuve@gmail.com>
2025-12-07 21:05:18 +0100

    debug

--- src/infrastructure/http/middleware/authMiddleware.ts
+++ src/infrastructure/http/middleware/authMiddleware.ts
@@ -9,6 +9,8 @@ export function authMiddleware(
   const authHeader = req.headers.authorization;
   const launcherHeader = req.headers.launcher;
 
+  console.log("Llega la petición:", req.headers);
+
   if (!checkIfHeadersExist(authHeader, launcherHeader)) {
     return res.status(401).json({ message: "No autorizado." });
   }
