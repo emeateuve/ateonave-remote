e5ffad26aee065282dff90b29c150d144a50b792
Miguel Ángel Torres Varo <emeateuve@gmail.com>
2025-12-26 19:22:48 +0100

    install wake_on_lan

--- src/infrastructure/http/controllers/wakeController.ts
+++ src/infrastructure/http/controllers/wakeController.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { Request, Response, NextFunction } from "express";
-import wol from "node-wol";
+import wol from "wake_on_lan";
 import { config } from "../../config/config";
 
 const router = Router();
@@ -18,18 +18,33 @@ router.post(
       return res.status(400).json({ message: "No hay broadcast configurado" });
 
     try {
-      await wol.wake(macAddress, { address: broadcastAddress }, (error) => {
+      await wol.wake(macAddress);
+
+      await wol.wake(macAddress, (error) => {
         if (error) {
           console.error("Error enviando el paquete WOL:", error);
-          return res
-            .status(500)
-            .json({
+          return res.status(500).json({
+            status: 500,
+            message: "Error enviando el paquete WOL",
+            error,
+          });
+        }
+      });
+      var magic_packet = wol.createMagicPacket(macAddress);
+      wol.sendMagicPacket(
+        magic_packet,
+        { address: broadcastAddress, port: 9 },
+        (error) => {
+          if (error) {
+            console.error("Error enviando el paquete WOL:", error);
+            return res.status(500).json({
               status: 500,
               message: "Error enviando el paquete WOL",
               error,
             });
+          }
         }
-      });
+      );
       return res.status(200).json({
         status: 200,
         message: "Paquete enviado con éxito, encendiendo la ateonave",
