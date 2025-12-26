ab7096b3910fa42909a06030d30f6e73e7946c5d
Miguel Ángel Torres Varo <emeateuve@gmail.com>
2025-12-26 20:44:48 +0100

    restore to wol

--- src/infrastructure/http/controllers/wakeController.ts
+++ src/infrastructure/http/controllers/wakeController.ts
@@ -1,60 +1,26 @@
 import { Router } from "express";
 import { Request, Response, NextFunction } from "express";
-import * as wol from "wake_on_lan";
+import wol from "wol";
 import { config } from "../../config/config";
-
 const router = Router();
-
 router.post(
   "/wake",
   async (req: Request, res: Response, next: NextFunction) => {
     const macAddress = config.mac;
-    const broadcastAddress = config.broadcast;
-
     if (!macAddress)
       return res.status(400).json({ message: "No hay mac configurada" });
-
-    if (!broadcastAddress)
-      return res.status(400).json({ message: "No hay broadcast configurado" });
-
     try {
       await wol.wake(macAddress);
-
-      await wol.wake(macAddress, (error) => {
-        if (error) {
-          console.error("Error enviando el paquete WOL:", error);
-          return res.status(500).json({
-            status: 500,
-            message: "Error enviando el paquete WOL",
-            error,
-          });
-        }
-      });
-      var magic_packet = wol.createMagicPacket(macAddress);
-      wol.sendMagicPacket(
-        magic_packet,
-        { address: broadcastAddress, port: 9 },
-        (error) => {
-          if (error) {
-            console.error("Error enviando el paquete WOL:", error);
-            return res.status(500).json({
-              status: 500,
-              message: "Error enviando el paquete WOL",
-              error,
-            });
-          }
-        }
-      );
       return res.status(200).json({
         status: 200,
         message: "Paquete enviado con éxito, encendiendo la ateonave",
       });
     } catch (err) {
-      return res
-        .status(500)
-        .json({ message: "Se ha producido un error al enviar el paquete" });
+      return res.status(500).json({
+        message: "Se ha producido un error al enviar el paquete",
+        err,
+      });
     }
   }
 );
-
 export default router;
