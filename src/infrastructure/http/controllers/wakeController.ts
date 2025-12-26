fe304428d2a9333a427f14836914de1bb95966a8
Miguel Ángel Torres Varo <emeateuve@gmail.com>
2025-12-26 19:12:02 +0100

    change wol to node-wol

--- src/infrastructure/http/controllers/wakeController.ts
+++ src/infrastructure/http/controllers/wakeController.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { Request, Response, NextFunction } from "express";
-import wol from "wol";
+import wol from "node-wol";
 import { config } from "../../config/config";
 
 const router = Router();
@@ -18,7 +18,18 @@ router.post(
       return res.status(400).json({ message: "No hay broadcast configurado" });
 
     try {
-      await wol.wake(macAddress, { address: broadcastAddress });
+      await wol.wake(macAddress, { address: broadcastAddress }, (error) => {
+        if (error) {
+          console.error("Error enviando el paquete WOL:", error);
+          return res
+            .status(500)
+            .json({
+              status: 500,
+              message: "Error enviando el paquete WOL",
+              error,
+            });
+        }
+      });
       return res.status(200).json({
         status: 200,
         message: "Paquete enviado con éxito, encendiendo la ateonave",
