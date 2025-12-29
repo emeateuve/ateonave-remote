a6aa50f3f87efafc8ddeef2da88029f585bb826b
Miguel Ángel Torres Varo <emeateuve@gmail.com>
2025-12-29 08:26:46 +0100

    ci:test

--- src/index.ts
+++ src/index.ts
@@ -28,5 +28,5 @@ app.use(wakeController);
 app.use(shutdownController);
 app.use(liveController);
 app.listen(config.api.port, "0.0.0.0", () => {
-  logger.info(`Ateonave remote listening on port ${config.api.port}`);
+  logger.info(`BOOYAKAH listening on port ${config.api.port}`);
 });
